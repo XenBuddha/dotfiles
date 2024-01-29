5a068ff98c514b07c4f2656b541147af9dff2f7f
gemstar <dave@gemstar.org>
2024-01-29 13:33:17 -0500

    Changed font installed

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ done
     sudo apt update && sudo apt -y install vim-scripts zsh zsh-syntax-highlighting zsh-autosuggestions
     echo "installing support apps..."
     sudo apt update && sudo apt -y install htop lsd bat vim git
-    echo "installing fonts-firacode..."
-    sudo apt update && sudo apt -y install fonts-firacode
+    echo "installing jetbrains mono fonts..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
     echo "installing tmux & support apps..."
     sudo apt update && sudo apt -y install tmux xclip
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm

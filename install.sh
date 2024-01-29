da5768c53a55d1e66071dc582b5dcd62268b0c9e
gemstar <dave@gemstar.org>
2024-01-28 20:57:57 -0500

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Find all dot files then if the original file exists, create a backup
+# Once backed up to {file}.dtbak symlink the new dotfile in place
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+
+# Install  vim-addons ZSH and ZSH extras
+    echo "installing vim & zsh extras..."
+    sudo apt update && sudo apt -y install vim-scripts zsh zsh-syntax-highlighting zsh-autosuggestions
+    echo "installing support apps..."
+    sudo apt update && sudo apt -y install htop lsd bat vim git
+    echo "installing fonts-firacode..."
+    sudo apt update && sudo apt -y install fonts-firacode
+    echo "installing tmux & support apps..."
+    sudo apt update && sudo apt -y install tmux xclip
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Installed"
+echo "use chsh -s /bin/zsh to switch to ZSH shell"
